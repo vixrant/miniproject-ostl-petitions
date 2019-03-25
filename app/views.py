382ffa5f7605b40e66cfe81e4ee9ef905c48c318
Vikrant Gajria <vikrantgajria@gmail.com>
2019-03-25 19:58:29 +0530

    Add index view

--- app/views.py
+++ app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from  .models import Petition
 
 def index(request):
-    return render(request, 'base.html')
+    petition_list = Petition.objects.all()
+    return render(request, 'index.html', {
+        "petition_list": petition_list
+    })
